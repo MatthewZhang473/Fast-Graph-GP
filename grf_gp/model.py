3ec1638dd12cd4c261acc9f225e1f0fdab8546df
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2026-01-17 19:13:05 +0000

    Initial Commit 4/N

--- grf_gp/model.py
+++ grf_gp/model.py
@@ -17,34 +17,53 @@ class GraphGP(gpytorch.models.ExactGP):
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-    def predict(self, x_test, n_samples=64):
-        train_indices = self.x_train.int().flatten()
-        test_indices = x_test.int().flatten()
-
+    def predict(self, x_test, batch_size=64):
+        x_train = self.x_train.int().flatten()
+        x_test = x_test.int().flatten()
         phi = self.covar_module._get_feature_matrix()
-        phi_train = phi[train_indices, :]
-        phi_test = phi[test_indices, :]
+        noise_std = torch.sqrt(
+            torch.tensor(self.likelihood.noise.item(), device=x_test.device)
+        )
+        return self._pathwise_conditioning(
+            x_train=x_train,
+            x_test=x_test,
+            phi=phi,
+            y_train=self.y_train,
+            noise_std=noise_std,
+            batch_size=batch_size,
+            device=x_test.device,
+        )
+
+    def _pathwise_conditioning(
+        self,
+        x_train,
+        x_test,
+        phi,
+        y_train,
+        noise_std,
+        batch_size,
+        device,
+    ):
+        """Pathwise conditioning to sample from the posterior."""
+        phi_train = phi[x_train, :]
+        phi_test = phi[x_test, :]
 
         K_train_train = phi_train @ phi_train.T
         K_test_train = phi_test @ phi_train.T
-
-        noise_variance = self.likelihood.noise.item()
-        noise_std = torch.sqrt(torch.tensor(noise_variance, device=x_test.device))
+        noise_variance = noise_std.pow(2)
         A = K_train_train + noise_variance * IdentityLinearOperator(
-            len(train_indices), device=x_test.device
+            phi_train.size(0), device=device
         )
 
-        eps1_batch = torch.randn(n_samples, self.num_nodes, device=x_test.device)
-        eps2_batch = noise_std * torch.randn(
-            n_samples, len(train_indices), device=x_test.device
+        eps_prior = torch.randn(batch_size, phi.size(0), device=device)
+        eps_obs = noise_std * torch.randn(batch_size, phi_train.size(0), device=device)
+
+        f_test_prior = eps_prior @ phi_test.T
+        f_train_prior = eps_prior @ phi_train.T
+
+        residual = y_train.unsqueeze(0) - (f_train_prior + eps_obs)
+        v = linear_cg(
+            A._matmul, residual.T, tolerance=gsettings.cg_tolerance.value()
         )
 
-        f_test_prior = eps1_batch @ phi_test.T
-        f_train_prior = eps1_batch @ phi_train.T
-
-        b_batch = self.y_train.unsqueeze(0) - (f_train_prior + eps2_batch)
-        v_batch = linear_cg(
-            A._matmul, b_batch.T, tolerance=gsettings.cg_tolerance.value()
-        )
-
-        return f_test_prior + (K_test_train @ v_batch).T
+        return f_test_prior + (K_test_train @ v).T
