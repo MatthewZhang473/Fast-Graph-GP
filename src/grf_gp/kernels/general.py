3472b6e01fd870ce7803c8cd75b8ecc03facf0d5
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2026-03-15 13:06:36 +0000

    [2/N] add low-rank general kernel

--- src/grf_gp/kernels/general.py
+++ src/grf_gp/kernels/general.py
@@ -1,6 +1,7 @@
 import torch
 
 from .base import BaseGRFKernel
+from .low_rank import LowRankGRFKernel
 
 
 class GRFGeneralKernel(BaseGRFKernel):
@@ -11,6 +12,23 @@ class GRFGeneralKernel(BaseGRFKernel):
     def __init__(self, rw_mats, max_walk_length: int, **kwargs):
         super().__init__(rw_mats=rw_mats, **kwargs)
         self.max_walk_length = max_walk_length
+        self.register_parameter(
+            name="raw_modulation_function",
+            parameter=torch.nn.Parameter(torch.randn(max_walk_length)),
+        )
+
+    @property
+    def modulation_function(self) -> torch.Tensor:
+        return self.raw_modulation_function
+
+
+class LowRankGeneralGRFKernel(LowRankGRFKernel):
+    def __init__(
+        self, rw_mats, max_walk_length: int, proj_dim: int, jlt_seed: int = 42, **kwargs
+    ):
+        super().__init__(
+            rw_mats=rw_mats, proj_dim=proj_dim, jlt_seed=jlt_seed, **kwargs
+        )
         self.register_parameter(
             name="raw_modulation_function",
             parameter=torch.nn.Parameter(torch.randn(max_walk_length)),
