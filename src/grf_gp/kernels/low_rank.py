8b07401bd51a8565b6acd48d62769d064c0caf60
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2026-03-15 12:39:46 +0000

    [1/N] low-rank GRF via JLT matrix

---
+++ src/grf_gp/kernels/low_rank.py
@@ -0,0 +1,26 @@
+import torch
+from abc import ABC
+
+from .base import BaseGRFKernel
+
+
+class LowRankGRFKernel(BaseGRFKernel, ABC):
+    def __init__(self, rw_mats, proj_dim: int, jlt_seed: int = 42, **kwargs):
+        super().__init__(rw_mats=rw_mats, **kwargs)
+        self.proj_dim = proj_dim
+        full_dim = rw_mats[0].size(-1)  # a.k.a. number of nodes in the graph
+        device = rw_mats[0].device
+
+        # Initialize the JLT projection matrix
+        gen = torch.Generator(device=device).manual_seed(jlt_seed)
+        jlt_proj = torch.randn(full_dim, proj_dim, generator=gen, device=device)
+        jlt_proj = jlt_proj * (proj_dim**-0.5)
+
+        self.register_buffer("jlt_proj", jlt_proj)
+
+    def _get_feature_matrix(self) -> torch.Tensor:
+        """
+        Calculates Phi_low_rank = Phi_full @ JLT_proj
+        """
+        phi_full = super()._get_feature_matrix()
+        return phi_full @ self.jlt_proj
