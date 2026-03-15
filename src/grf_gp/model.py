cfdaf46ef890764322427ebbdc513f94652dd4c6
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2026-03-15 21:12:51 +0000

    [2/N] Also the LowRankGRFGP &some big changes in naming!
    cleanup

--- src/grf_gp/model.py
+++ src/grf_gp/model.py
@@ -3,7 +3,8 @@ import gpytorch
 from abc import ABC, abstractmethod
 from linear_operator.operators import DiagLinearOperator
 from .kernels.base import BaseGRFKernel, BaseExactKernel
-from .inference import pathwise_conditioning
+from .kernels.low_rank import LowRankGRFKernel
+from .inference import pathwise_conditioning, woodbury_pathwise_conditioning
 
 
 class GraphGP(gpytorch.models.ExactGP, ABC):
@@ -30,15 +31,15 @@ class GraphGP(gpytorch.models.ExactGP, ABC):
         pass
 
 
-class GraphGRFGP(GraphGP):
+class GRFGP(GraphGP):
     def predict_sample(self, x_test, n_samples=64):
         with torch.no_grad():
             x_train = self.x_train.int().flatten()
             x_test = x_test.int().flatten()
             phi = self.covar_module._get_feature_matrix()
-            noise_std = torch.sqrt(
-                torch.tensor(self.likelihood.noise.item(), device=x_test.device)
-            )
+            noise_std = self.likelihood.noise.to(
+                device=x_test.device, dtype=phi.dtype
+            ).sqrt()
             return pathwise_conditioning(
                 x_train=x_train,
                 x_test=x_test,
@@ -58,7 +59,32 @@ class GraphGRFGP(GraphGP):
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
 
-class GraphExactGP(GraphGP):
+class LowRankGRFGP(GRFGP):
+    def __init__(self, x_train, y_train, likelihood, kernel: LowRankGRFKernel):
+        super().__init__(x_train, y_train, likelihood, kernel)
+        if not isinstance(self.covar_module, LowRankGRFKernel):
+            raise TypeError("LowRankGRFGP requires a LowRankGRFKernel.")
+
+    def predict_sample(self, x_test, n_samples=64):
+        with torch.no_grad():
+            x_train = self.x_train.int().flatten()
+            x_test = x_test.int().flatten()
+            phi = self.covar_module._get_feature_matrix()
+            noise_std = self.likelihood.noise.to(
+                device=x_test.device, dtype=phi.dtype
+            ).sqrt()
+            return woodbury_pathwise_conditioning(
+                x_train=x_train,
+                x_test=x_test,
+                phi=phi,
+                y_train=self.y_train,
+                noise_std=noise_std,
+                batch_size=n_samples,
+                device=x_test.device,
+            )
+
+
+class ExactGraphGP(GraphGP):
     def predict(self, x_test, **kwargs):
         self.eval()
         self.likelihood.eval()
