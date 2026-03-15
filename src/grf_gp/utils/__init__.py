8be3ce7c106836b1223057b59754c2f2b0a323f4
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2026-03-15 21:30:10 +0000

    also some useful functions

--- src/grf_gp/utils/__init__.py
+++ src/grf_gp/utils/__init__.py
@@ -1,3 +1,11 @@
+from .config import set_gp_defaults
+from .sampling import generate_noisy_samples
+from .spectral import get_normalized_laplacian
 from .sparse_lo import SparseLinearOperator
 
-__all__ = ["SparseLinearOperator"]
+__all__ = [
+    "SparseLinearOperator",
+    "generate_noisy_samples",
+    "get_normalized_laplacian",
+    "set_gp_defaults",
+]
